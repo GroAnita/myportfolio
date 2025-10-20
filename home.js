853cc451912bae27fcde1cc296dc6a4e59b86d71
GroAnita <gro.anita.brathen@gmail.com>
2025-10-20 14:48:23 +0200

    Add contact me page and update portfolio
    - Added contactme.html with contact form
    - Created contactme.css for styling
    - Updated navigation components
    - Added logo images
    - Cleaned up form files

---
+++ home.js
@@ -0,0 +1,8 @@
+const linkBoxes = document.querySelector('.home-container__box')
+const linkBox1 = document.querySelector('.home-container__box--1')
+const linkBox2 = document.querySelector('.home-container__box--2')  
+const linkBox3 = document.querySelector('.home-container__box--3')
+const linkBox4 = document.querySelector('.home-container__box--4')  
+
+linkBox3.onclick = () => window.location.href = 'mywork.html';
+    

bb3c7ac0f1505e9390beb7987656b81aab5a7a5a
GroAnita <gro.anita.brathen@gmail.com>
2025-10-22 09:22:38 +0200

    some small changes here and there

--- home.js
+++ home.js
@@ -4,10 +4,12 @@ const linkBox2 = document.querySelector('.home-container__box--2')
 const linkBox3 = document.querySelector('.home-container__box--3')
 const linkBox4 = document.querySelector('.home-container__box--4')  
 
-linkBox3.onclick = () => window.location.href = 'mywork.html';
 
-linkBox4.onclick = () => window.location.href = 'contactme.html';
     
 linkBox1.onclick = () => window.location.href = 'whoamI.html';
 
 linkBox2.onclick = () => window.location.href = 'whatcanIdo.html';
+
+linkBox3.onclick = () => window.location.href = 'mywork.html';
+
+linkBox4.onclick = () => window.location.href = 'contactme.html';

1b5cb0eadba82fcffa700b725fb6da55b0ebf21c
GroAnita <gro.anita.brathen@gmail.com>
2025-10-22 13:35:34 +0200

    some changes with links and added a few aria labels

--- nav-component.js
+++ nav-component.js
@@ -1,22 +1,32 @@
 function createNavOverlay() {
     return `
         <div class="nav-overlay" id="navOverlay">
-            <button class="nav-overlay__close-button" id="closeNavBtn">&times;</button>
+            <button class="nav-overlay__close-button" id="closeNavBtn" aria-label="Close navigation menu">&times;</button>
             <nav class="nav-overlay__links">
                 <a href="index.html" class="nav-overlay__link">Home</a>
-                <a href="aboutme.html" class="nav-overlay__link">About Me</a>
+                <a href="whoamI.html" class="nav-overlay__link">About Me</a>
                 <a href="mywork.html" class="nav-overlay__link">My Work</a>
                 <a href="contactme.html" class="nav-overlay__link">Contact Me</a>
             </nav>
             <hr class="nav-overlay__divider">
             <div class="nav-overlay__div">
-                <div class="nav-overlay__logo"><img src="images/GABlogocatpink.svg" alt="Logo"></div>
+                <div class="nav-overlay__logo"><img src="images/GABlogocatwhitetext.svg" alt="Logo for Gro Anita Bråthen, a black cat with text: PawsitiveVibesByGro"></div>
                 <div class="nav-overlay__some"> 
-                    <i class="fa-brands fa-facebook-f"></i>
-                    <i class="fa-brands fa-instagram"></i>
-                    <i class="fa-brands fa-tiktok"></i>
-                    <i class="fa-brands fa-linkedin-in"></i>
-                    <i class="fa-brands fa-github"></i>
+                    <a href="https://www.facebook.com/Vampyrellan" target="_blank" rel="noopener noreferrer" aria-label="Visit my Facebook page">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                    <a href="https://www.instagram.com/pawsitivevibesbygro/" target="_blank" rel="noopener noreferrer" aria-label="Visit my Instagram page">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer" aria-label="Visit my TikTok page">
+                        <i class="fa-brands fa-tiktok"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/gro-anita-brathen-2a0513154/" target="_blank" rel="noopener noreferrer" aria-label="Visit my LinkedIn page">
+                        <i class="fa-brands fa-linkedin-in"></i>
+                    </a>
+                    <a href="https://github.com/GroAnita" target="_blank" rel="noopener noreferrer" aria-label="Visit my GitHub page">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
                 </div>
             </div>
         </div>
@@ -25,7 +35,7 @@ function createNavOverlay() {
 
 function createHamburgerMenu() {
     return `
-        <button class="hamburger-menu">
+        <button class="hamburger-menu" aria-label="Open navigation menu">
             <i class="fa-solid fa-bars"></i>
         </button>
     `;
